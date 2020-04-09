8cf92fbb8ff19812d296808703e34bb923f58148
Leboda <hamilton.mandimby@gmail.com>
2020-04-10 01:34:35 +0300

    First Commit

---
+++ application/views/template.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Rakitra</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url(); ?>assets/img/favicon.png" rel="icon">
+  <link href="<?php echo base_url(); ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/vendor/owl.carousel/<?php echo base_url(); ?>assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
+  
+  <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
+  <!-- =======================================================
+  * Template Name: Personal - v2.0.1
+  * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header-tops">
+    <div class="container">
+
+      <h1><a href="index.html">RAKITRA</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html"><img src="<?php echo base_url(); ?>assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      <h2>"Samia manao araka izay efa nokasainy tao am-pony avy, tsy amin'alahelo na an-tery, fa ny manome an-kafaliana no sitrak'Andriamanitra" <span>2 Korintiana 9,7</span></h2>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="#header">Tongasoa</a></li>
+          <li><a href="#rakitra">Hanao rakitra</a></li>
+          <li><a href="#mombamomba">Mombamomba</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+    <?=$section?>
+
+  
+
+  <div class="credits">
+    Copyright ® | 2020  <a href="https://www.univ-fianar.mg">Université de Fianarantsoa</a>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/counterup/counterup.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/venobox/venobox.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ application/views/tongasoa.php
@@ -0,0 +1,91 @@
+  <!-- ======= Contact Section ======= -->
+  <section id="rakitra" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Hanao rakitra</h2>
+        
+      </div>
+      <p>Mitohy hatrany ny asan'ny Fiangonana amin'ny fanompoana anan'Andriamanitra. </p>
+
+
+      <form action="forms/contact.php" method="post" role="form" class="php-email-form mt-4">
+        
+        <div class="form-row">
+          <div class="col-md-12 form-group">
+            <select name="service" id="service" class="form-control selectpicker" data-live-search="true" placeholder="Fiangonana misy anao" >
+              <option value="">EKAR Jean Paul 2 Ampahidrano</option>
+              <option value="">FLM Mahamanina Fiadanana</option>
+            </select>
+          </div> 
+        </div>       
+        <div class="form-row">
+          <div class="col-md-6 form-group">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Anarana feno"  data-msg="Ampidiro ny Anaranao" />
+            <div class="validate"></div>
+          </div>
+          <div class="col-md-6 form-group">
+            <input type="text" class="form-control" name="tel" id="tel" placeholder="Laharana Finday"  data-msg="Ampidiro ny laharana Finday" />
+            <div class="validate"></div>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="col-md-12 form-group">
+            <input type="text" name="adresse" class="form-control" id="adresse" placeholder="Toerana misy anao (Adiresy Mazava)" data-rule="minlen:4" data-msg="Ny toerana misy anao" />
+            <div class="validate"></div>
+          </div>
+          <div class="col-md-12 form-group">
+            <input type="email" name="email" class="form-control" id="email" placeholder="adiresy-mailaka@mmmm.mm" data-rule="email" data-msg="Ampidiro ny adiresy mailaka" />
+            <div class="validate"></div>
+          </div>
+        </div>
+        <div class="text-center"><button type="submit">Handrotsaka</button></div>
+      </form>
+
+    </div>
+  </section><!-- End Contact Section -->
+
+  <!-- ======= About Section ======= -->
+  <section id="mombamomba" class="about">
+
+    <!-- ======= About Me ======= -->
+    <div class="about-me container">
+
+      <div class="section-title">
+        <h2>Mombamomba</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="assets/img/uf.png" class="img-thumbnail" alt="Logo univ-fianar">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>Tetikasa RAKITRA</h3>          
+          <p>
+            Ity vokatra arateknolojia ity dia nivoaka tao anatin'izao "Fihiboana" na koa mijanona an-trano izao, izay fanampahakevitra noraisin'ny Fanjakana sy ny Fiangonana na ny Fikambanam-mpiangonana misy eto Madagasikara.
+          </p>
+          <p>
+            Maro amintsika kristianina na finoana hafa anefa no te hanampy ny Fiangonana izay misy azy tsirairay avy. Anton'izany no nananganana an'ity tetikasa RAKITRA ity.
+          </p>
+          <p>
+            Raha misy toromarika, torohevitra na fanampiana dia manaraka eto ny fifandraisana: 
+          </p>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="icofont-rounded-right"></i> <strong>Adiresy:</strong> Oniverisiten'ny Fianarantsoa</li>
+                <li><i class="icofont-rounded-right"></i> <strong>Toerana:</strong> Andrainjato</li>
+                <li><i class="icofont-rounded-right"></i> <strong>Mailaka:</strong> dtic.univfianar@gmail.com</li>
+              </ul>
+            </div>
+          </div>
+          <p class="font-italic">
+            Fiarahamiasa teo amin'ny Oniversiten'ny Fianarantsoa sy ny Orange Madagascar (Orange Money)
+          </p>
+        </div>
+      </div>
+
+    </div><!-- End About Me -->
+
+
+  </section><!-- End About Section -->
