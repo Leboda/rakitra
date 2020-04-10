5dbd4991210d0b605c91363f04b779dbd99517dc
antihiesotra <alexjob.travail@gmail.com>
2020-04-10 10:54:46 +0300

    Configuration librairy template

--- application/controllers/Tongasoa.php
+++ application/controllers/Tongasoa.php
@@ -2,9 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Tongasoa extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->templates = 'template';
+	}
 
 	public function index()
 	{
-		$this->load->view('tongasoa');
+		$this->template->set('title','Tongasoa');
+		$this->template->load($this->templates,'tongasoa');
 	}
 }
